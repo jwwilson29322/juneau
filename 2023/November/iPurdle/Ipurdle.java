cc18642da2b3b21868db45a7bc69d86b7fdf82dc
JW Wilson <jwwilson29322@yahoo.com>
2023-11-19 12:53:48 -0500

    add ask to play again

--- 2023/November/iPurdle/Ipurdle.java
+++ 2023/November/iPurdle/Ipurdle.java
@@ -1,24 +1,116 @@
-
+import java.util.Scanner;
 public class Ipurdle {
     public static void main(String[] args) {
-        //MAIN GOES HERE
+        Scanner scr = new Scanner(System.in);
+        int wordSize = 5;
+        int maxAttempts = 6;
+        if (args!=null && args.length>0 && args[0] != null) {
+            wordSize = Integer.parseInt(args[0]);
+        }
+        if (args!=null && args.length>0 && args[1] != null) {
+            maxAttempts = Integer.parseInt(args[1]);
+        }
+        welcomeMessage(wordSize, maxAttempts);
+        String guess = "";
+        DictionaryIP gameWordsDictionary = new DictionaryIP(wordSize);
+        DictionaryIP puzzlesDictionary = new DictionaryIP(wordSize);
+        do{
+            guess = askForGuess(gameWordsDictionary, scr);
+
+            System.out.print("Palavra com a pista > ");
+            printClue(playGuess(puzzlesDictionary, guess), guess);
+            System.out.println("");
+            if (isMaxClue(playGuess(puzzlesDictionary, guess), wordSize)) {
+                System.out.println("");
+                System.out.println("");
+                System.out.println("Parabens, encontraste a palavra secreta!!!");
+                end(scr);
+                //END
+            } else {
+                maxAttempts -=1;
+            }
+        } while (maxAttempts > 0);
+        if (!isMaxClue(playGuess(puzzlesDictionary, guess), wordSize)) {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("YOU LOST");
+            end(scr);
+        };
+    }
+
+    private static void askToPlayAgain(){
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Would you like to play again?  Y/N ");
+        System.out.println("");
+        System.out.println("");
+    }
+
+    public static void end(Scanner scr){
+        askToPlayAgain();
+        String input = scr.nextLine();
+        if (input.equals("Y") || input.equals("y")){
+            main(null);
+        }else {
+            System.exit(0);
+        }
+    }
+
+    public static String askForGuess(DictionaryIP gameWordsDictionary, Scanner scr) {
+        boolean validWord = false;
+        String guess = "";
+        do {
+            System.out.print("Palavra a jogar? ");
+            guess = scr.nextLine();
+            guess = guess.toUpperCase();
+
+            int size = guess.length();
+            int wordSize = gameWordsDictionary.getWordSize();
+            if (guess.equals("QUIT")) {
+                //END
+                end(scr);
+            } else if (!gameWordsDictionary.isValid(guess)) {
+                System.out.println("Palavra invalida, nao existe no dicionario");
+            } else if (size != wordSize) {
+                System.out.println("Palavra invalida, tamanho errado");
+            } else {
+                validWord = true;
+            }
+        } while (!validWord);
+        return guess;
+    }
+
+
+    public static void welcomeMessage(int wordSize, int maxAttempts) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Bem vindo ao jogo Ipurdle de Juneau Wilson e Dienis Garkavenko!");
+        System.out.println("Neste jogo as palavras tem tamanho " + wordSize + ". O dicionario tem apenas palavras em ingles relacionadas com IP.");
+        System.out.println("Tens " + maxAttempts + " tentativas para adivinhar a palavra. Boa sorte!");
+        System.out.println("");
+        System.out.println("");
     }
 
     /**
      * Checks if {@code clue} is valid.
-     *
+     * 
      * @param clue numerical representation of characters correct, incorrect, or in wrong position in word.
      * @param size length of words chosen for this game.
      * @return true if {@code clue} is a valid representation for corresponding word length.
      */
     public static boolean validClue(int clue, int size) {
         int clueLength = String.valueOf(clue).length();
-        return clueLength == size && clue > 0 && isTernary(clue, size);
+ 
+ 
+        if (clueLength == size && clue > 0 && isTernary(clue, size)) {
+            return true;
+        } else
+        return false;
     }
 
     /**
      * Checks if {@code clue} is a valid ternary number.
-     *
+     *  
      * @param clue decimal integer representation of possible ternary number.
      * @param size length of words chosen for this game.
      * @return true if {@code clue} is a valid base three number.
@@ -34,14 +126,29 @@ public class Ipurdle {
         return true;
     }
 
+    /**
+     * Creates a numerical clue that corresponds to the minimum clue possible. 
+     * 
+     * @param size length of words chosen for this game.
+     * @requires {@code size} is a number larger than 0.
+     * @return numerical representation of the minimum clue possible.
+     */
     public static int minClue(int size){
         int clue = 0;
         for(int i= 0; i < size; i ++){
-            clue = (clue * 10) + 1;
+           clue = (clue * 10) + 1;
         }
         return clue;
     }
 
+    /**
+     * Checks if the maximum clue with {@code clue} and {@code size} is valid.
+     * 
+     * @param clue numerical representation of characters correct, incorrect, or in wrong position in word.
+     * @param size length of words chosen for this game.
+     * @requires {@code size} is number larger than 0 and {@code clue} is a valid representation for corresponding word length.
+     * @return true if numerical representation of correct characters is valid given a {@code clue} and {@code size}.
+     */
     public static boolean isMaxClue(int clue, int size){
         boolean maxClue = true;
         int tst = 0;
@@ -56,7 +163,7 @@ public class Ipurdle {
 
     /**
      * Calculates smallest {@code clue} larger than current {@code clue}; adds one to {@code clue}.
-     *
+     * 
      * @param clue numerical representation of characters correct, incorrect, or in wrong position in word.
      * @param size length of words chosen for this game.
      * @return numerical representation of {@code clue} plus one.
@@ -79,8 +186,32 @@ public class Ipurdle {
     }
 
     /**
+     * Creates a visual colored representation of {@code guess} given the correct numerical representation for that word, being green the correct letter and position, 
+     * being yellow the correct letter and wrong position and being black the wrong position and letter.
+     * 
+     * @param clue numerical representation of characters correct, incorrect, or in wrong position in word.
+     * @param guess word input by player in this round of game.
+     * @requires {@code guess} being different than null and {@code clue} being the valid numerical representation of {@code guess}.
+     * @return the colored version of {@code guess} in accordance of {@code clue}.
+     */
+    public static void printClue(int clue, String guess) {
+        String s = String.valueOf(clue);
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < s.length(); i ++) {
+            if (s.charAt(i) == '1') {
+                str.append(StringColouring.toColoredString(String.valueOf(guess.charAt(i )), StringColouring.BLACK));
+            } else if (s.charAt(i) == '2') {
+                str.append(StringColouring.toColoredString(String.valueOf(guess.charAt(i)), StringColouring.YELLOW));
+            } else {
+                str.append(StringColouring.toColoredString(String.valueOf(guess.charAt(i)), StringColouring.GREEN));
+            }
+        }
+        System.out.print(str);
+    }
+
+    /**
      * Creates a numerical clue that corresponds to the guess and word input
-     *
+     * 
      * @param guess word input by player in this round of game.
      * @param word correct word that player is trying to guess.
      * @requires guess and word have same number of characters, guess and word all uppercase.
@@ -110,23 +241,14 @@ public class Ipurdle {
         return Integer.parseInt(clue.toString());
     }
 
-    private static DictionaryIP cloneDictionary(DictionaryIP dict1){
+    public static DictionaryIP cloneDictionary(DictionaryIP dict1) {
         String[] dict2Words = new String[dict1.lenght()];
-        for (int x = 0; x<dict1.lenght(); x++){
+        for (int x = 0; x < dict1.lenght(); x++){
             dict2Words[x] = dict1.getWord(x);
         }
         return new DictionaryIP(dict1.getWordSize(), dict2Words);
     }
 
-    /**
-     * Calculates number of valid possible words with {@code clue} and {@code guess}.
-     *
-     * @param dict dictonary of all valid words.
-     * @param clue numerical representation of characters correct, incorrect, or in wrong position in word.
-     * @param guess word input by player in this round of game.
-     * @return number of possible valid words.
-     * @requires {@code clue} be a valid clue for {@code guess}.
-     */
     public static int howManyWordsWithClue(DictionaryIP dict, int clue, String guess) {
         DictionaryIP dict2 = cloneDictionary(dict);
         checkThrees(dict2, guess, clue);
@@ -135,7 +257,7 @@ public class Ipurdle {
         return dict2.lenght();
     }
 
-    private static void checkOnes(DictionaryIP dict, String guess, int clue) {
+    public static void checkOnes(DictionaryIP dict, String guess, int clue) {
         for (int i = 0; i < guess.length(); i++) {
             if ((Integer.toString(clue).charAt(i))=='1') {
                 for (int l = 0; l < dict.lenght(); l++) {
@@ -150,7 +272,7 @@ public class Ipurdle {
         }
     }
 
-    private static void checkTwos(DictionaryIP dict, String guess, int clue) {
+    public static void checkTwos(DictionaryIP dict, String guess, int clue) {
         for (int i = 0; i < guess.length(); i++) {
             if ((Integer.toString(clue).charAt(i))=='2') {
                 for (int k = 0; k < dict.lenght(); k++) {
@@ -171,11 +293,11 @@ public class Ipurdle {
                     }
                 }
                 dict.removeSelected();
-            }
+            }  
         }
     }
 
-    private static void checkThrees(DictionaryIP dict, String guess, int clue) {
+    public static void checkThrees(DictionaryIP dict, String guess, int clue) {
         for (int i = 0; i < guess.length(); i++) {
             if ((Integer.toString(clue).charAt(i))=='3') {
                 for (int j = 0; j < dict.lenght(); j++) {
@@ -190,15 +312,21 @@ public class Ipurdle {
 
     /**
      * Checks if a certain character input is in a chosen string in a specific position.
-     *
+     * 
      * @param c character being checked against word.
      * @param wordToCheck word being checked for presence of {@code c}.
      * @param pos position in which {@code c} must be in in the word.
      * @return true if {@code c} is in {@code wordToCheck} in {@code pos}.
      * @requires {@code pos} must be less than length of {@code wordToCheck}.
      */
-    private static boolean checkThisLetter(char c, String wordToCheck, int pos) {
-        return wordToCheck.charAt(pos)==c;
+    public static boolean checkThisLetter(char c, String wordToCheck, int pos) {
+        boolean letterInWord;
+        if (wordToCheck.charAt(pos)==c) {
+            letterInWord = true;
+        } else {
+            letterInWord = false;
+        }
+        return letterInWord;
     }
 
     public static int betterClueForGuess(DictionaryIP dict, String guess) {
@@ -217,14 +345,22 @@ public class Ipurdle {
                 clueMax = secondClue;
             }
             clueStart = nextClue(clueStart, guess.length());
-        }
+        }   
         return clueMax;
     }
-
-    public static int playGuess(DictionaryIP dict, String guess){
+    
+    /**
+    * Calculates the best clue for the {@code guess} using a previous method, betterClueForGuess, and removes all the other words that don´t result in this clue.
+    *
+    * @param dict dictonary of all valid words.
+    * @param guess word input by player in this round of game.
+    * @requires {@code guess} and {@code dict} to be different than null.
+    * @return the best clue for {@code guess} facing the {@code dict}.
+    */
+    public static int playGuess(DictionaryIP dict, String guess) {
         int clueForGuess = betterClueForGuess(dict, guess);
-        for(int i = 0; i<dict.lenght(); i++){
-            if(clueForGuessAndWord(guess, dict.getWord(i)) != clueForGuess){
+        for (int i = 0; i < dict.lenght(); i++) {
+            if (clueForGuessAndWord(guess, dict.getWord(i)) != clueForGuess) {
                 dict.selectForRemove(i);
             }
         }
